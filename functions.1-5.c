2c87e4f0584b59c11cb3f3d7f30cc5e8671bb530
Dak4rPrO <shinoflow111@gmail.com>
2022-03-12 08:27:01 -0800

    arreglos de betty function.1-5.c

--- functions.1-5.c
+++ functions.1-5.c
@@ -4,71 +4,74 @@
 *_printf_c - funcion que corresponde a printf %c
 *
 *@caracter: caracter a printear perteneciente a la tabla ASCII
+*Return: 1
 */
 
 int _printf_c(char caracter)
 {
-    if (caracter >= 36 && caracter <= 126)
-    {
-        _putchar(caracter);
-        _putchar('\n');
-    }
-    return (1);
+	if (caracter >= 36 && caracter <= 126)
+	{
+		_putchar(caracter);
+		_putchar('\n');
+	}
+	return (1);
 }
 
 /**
 *_printf_s - function that prints a string
 *
 *@str: string to print
+*Return: i
 */
 int _printf_s(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (*(str + i) != 0)
-    {
-        _putchar(*(str + i));
-        i++;
-    }
-    _putchar('\n');
-    return (i);
+	while (*(str + i) != 0)
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+	return (i);
 }
 
-/*
+/**
 *_printf_d_i - function that prints an integer.
 *
 *@n: number given.
+*
+*Return: i
 */
-
 int _printf_d_i(int n)
 {
-        int i;
+	int i;
 	unsigned int dc, dig, nat = n;
-        double f = 1;
+	double f = 1;
 
-        if (n == 0)
-                _putchar('0');
-        else
-        {
-                if (n < 0)
-                {
-                        nat = n * -1;
-                        _putchar('-');
-                }
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			nat = n * -1;
+			_putchar('-');
+		}
 
-                while (f <= nat)
-                        f *= 10;
-                dc = f / 10;
+		while (f <= nat)
+			f *= 10;
+		dc = f / 10;
 
-                while (dc >= 1)
-                {
-                        dig = nat / dc;
-                        _putchar(dig + '0');
-                        nat = (nat - (dc * dig));
-                        dc /= 10;
-                }
-        }
-        _putchar('\n');
+		while (dc >= 1)
+		{
+			dig = nat / dc;
+			_putchar(dig + '0');
+			nat = (nat - (dc * dig));
+			dc /= 10;
+		}
+	}
+	_putchar('\n');
 
 	for (i = 0; i < n; i++)
 		return (i);
@@ -76,39 +79,40 @@ int _printf_d_i(int n)
 
 /**
  * _printf_u - function that prints an unsigned integer
- * @a: number
+ * @n: number
+ * Return: i
  */
 int _printf_u(unsigned int n)
 {
 	int i;
 	unsigned int dc, dig, nat = n;
-        double f = 1;
+	double f = 1;
 
-        if (n == 0)
-                _putchar('0');
-        else
-        {
-                while (f <= nat)
-                        f *= 10;
-                dc = f / 10;
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		while (f <= nat)
+			f *= 10;
+		dc = f / 10;
 
-                while (dc >= 1)
-                {
-                        dig = nat / dc;
-                        _putchar(dig + '0');
-                        nat = (nat - (dc * dig));
-                        dc /= 10;
-                }
-        }
-        _putchar('\n');
+		while (dc >= 1)
+		{
+			dig = nat / dc;
+			_putchar(dig + '0');
+			nat = (nat - (dc * dig));
+			dc /= 10;
+		}
+	}
+	_putchar('\n');
 	for (i = 0; i < n; i++)
-		 return (i);
+		return (i);
 }
 
 /**
- * print_r - function that prints a string in reverse
+ * _printf_r - function that prints a string in reverse
  * @str : ponter
- * _putchar - si
+ * Return: a
 */
 int _printf_r(char *str)
 {
