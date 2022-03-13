65d0734c61caa30959cbda961c904772122d0812
Titania792 <darkwarrior792@gmail.com>
2022-03-12 17:16:44 -0800

    elimino % de get_functions.c

--- get_functions.c
+++ get_functions.c
@@ -8,20 +8,20 @@
 int _get_functions(va_list list)
 {
 	op_t ops[] = {
-		{"%c", _printf_c},
-		{"%s", _printf_s},
-		{"%d", _printf_d_i},
-		{"%i", _printf_d_i},
-		{"%u", _printf_u},
-		{"%r", _printf_r},
-		{"%R", _printf_13},
-		{"%x", _printf_x},
-		{"%X", _printf_X},
-		{"%o", _printf_o},
-		{"%b", _printf_b},
-		{"%p", _printf_p},
-		{"%h", _printf_h},
-		{"%%", _printf_por},
+		{"c", _printf_c},
+		{"s", _printf_s},
+		{"d", _printf_d_i},
+		{"i", _printf_d_i},
+		{"u", _printf_u},
+		{"r", _printf_r},
+		{"R", _printf_13},
+		{"x", _printf_x},
+		{"X", _printf_X},
+		{"o", _printf_o},
+		{"b", _printf_b},
+		{"p", _printf_p},
+		{"h", _printf_h},
+		{"%", _printf_por},
 		{NULL, NULL}
 	};
 
